b00cce66bc5cb6e9bc3e9c5e4c6fb0997ea02e33
Maui Höfferle <94994110+mhofferle@users.noreply.github.com>
2021-11-29 11:52:52 -0300

    subo boletines

---
+++ boletines/javascript/main.js
@@ -0,0 +1,23 @@
+//DOM
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function(link){
+    link.addEventListener("click",function(ev){
+      ev.preventDefault();
+      let content = document.querySelector(".content");
+
+      content.classList.remove("animate__fadeIn");
+      content.classList.remove("animated");
+
+      content.classList.add("animate__fadeOutUp");
+      content.classList.add("animated");
+
+      setTimeout(function(){
+        location.href = "/";
+      },600);
+
+      return false;
+
+    });
+});
